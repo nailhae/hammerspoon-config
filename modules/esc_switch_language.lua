2c57ea531851210a1b982cb6598a7ba21ae5fae1
IH-M2 <ilhae.na@gmail.com>
2022-12-24 00:10:42 +0900

    add esc_switch_language reference

--- modules/esc_switch_language.lua
+++ modules/esc_switch_language.lua
@@ -1,3 +1,4 @@
+-- ref: https://frhyme.github.io/hammerspoon/hammerspoon00_use_esc_to_input_en/
 -- 이건 일종의 리스너륾 만든거임. 키가 입력되면 걔가 특정 키인지 확인하고, 해당되는 행위를 수행하도록 함.
 -- hs.eventtap.event.types.keyDown: modifier key가 아닌 다른 일반 key에 down이 발생했을 때
 -- 입력된 key의 code가 'escape'이면 input_source를 확인하고 영어가 아닐 경우 영어로 변환해준다.
